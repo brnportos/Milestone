aa224ea91a6ee020c2e57631014aabb7850488e2
brnportos <brnoportos@gmail.com>
2026-05-10 21:27:09 +0300

    feat(push_swap): add is_stack_len implementation

--- push_swap/operations/is_Chunk_sorting.c
+++ push_swap/operations/is_Chunk_sorting.c
@@ -12,11 +12,31 @@ int is_sorted(t_stack *a)
     }
     return (1);
 }
+
+int is_stack_len(t_stack *a)
+{
+    int len;
+    t_stack *head;
+
+    if (!a)
+        return (0);
+    len = 0;
+    head = a;
+    while (head)
+    {
+        len++;
+        head = head->next;
+    }
+    return (len);
+}
+
 void    is_chunk_sorting(t_stack **a, t_stack **b, t_stats *ops)
 {
-    int size;
+    int len;
     int chunk_size;
 
     if (!*a || is_sorted(*a) == 1)
         return ;
+    len = is_stack_len(*a);
+
 }
