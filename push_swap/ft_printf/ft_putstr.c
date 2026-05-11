c6ce7ce22c6c75699f6ed2f136836927380e545d
brnportos <brnoportos@gmail.com>
2026-05-11 05:40:47 +0300

    feat(push_swap): add ft_printf implementation

---
+++ push_swap/ft_printf/ft_putstr.c
@@ -0,0 +1,20 @@
+#include "ft_printf.h"
+
+void	ft_putstr(int fd, char *str, int *len)
+{
+	int	i;
+
+	i = 0;
+	if (!len)
+		return ;
+	if (!str)
+	{
+		ft_putstr(fd, "(null)", len);
+		return ;
+	}
+	while (str[i])
+	{
+		ft_putchar(fd, str[i], len);
+		i++;
+	}
+}
